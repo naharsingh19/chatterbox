da0907273db42536d2137c207113e6c812786b35
Nahar <nahartewatia19@gmail.com>
2024-04-09 23:36:44 +0530

    made responsive

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -3,10 +3,10 @@ import Sidebar from "../../components/sidebar/Sidebar";
 
 const Home = () => {
 	return (
-		<div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
-			<Sidebar />
-			<MessageContainer />
-		</div>
-	);
+    <div className='flex flex-col sm:flex-row sm:h-[450px] md:h-[550px] rounded-lg overflow-auto bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+      <Sidebar className="sm:w-full sm:h-1/4 md:w-1/4 md:h-full" />
+      <MessageContainer className="sm:w-full sm:h-3/4 md:w-3/4 md:h-full" />
+    </div>
+  );
 };
 export default Home;
